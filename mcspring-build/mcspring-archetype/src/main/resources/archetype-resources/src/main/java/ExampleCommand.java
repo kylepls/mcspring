d760c7ddc3d4875589023897d540743262f09f2c
kylepls <mail@kyle.in>
2020-02-27 01:15:14 -0700

    Add more docs to ExampleCommand.java

--- mcspring-build/mcspring-archetype/src/main/resources/archetype-resources/src/main/java/ExampleCommand.java
+++ mcspring-build/mcspring-archetype/src/main/resources/archetype-resources/src/main/java/ExampleCommand.java
@@ -6,14 +6,23 @@ import javax.annotation.PostConstruct;
 
 import in.kyle.mcspring.command.Command;
 
+// Remember to annotate Spring beans with @Component
+// If you forget this, your class will not run.
 @Component
 class ExampleCommand {
+    // Do not extend JavaPlugin, you will regret it
+    // To get an instance of Plugin, have Spring inject it.
     
+    // Use this in-place of an onEnable method. You can make as many of these methods as you like.
+    // for onDisable see the @PreDestroy annotation.
     @PostConstruct
     void onEnable() {
     
     }
     
+    // Commands will be automatically set up. Do not create a plugin.yml
+    // The return value of a command method will be sent to the CommandSender using Object::toString
+    // void methods simplly will not send a message
     @Command("test")
     String test() {
         return "The command works!";
